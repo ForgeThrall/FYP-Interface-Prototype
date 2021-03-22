fa88914f581d83b28d4e5b2d7761ef602008ac04
ForgeThrall <forgemastersemail@gmail.com>
2021-03-22 16:11:23 -0500

    skeleton setup

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import initialData from './initialData';
+import { DragDropContext } from 'react-beautiful-dnd';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class App extends React.Component {
+	state = initialData;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+	onDragEnd = result => { /* todo */ }
+
+	render() {
+		return (
+			<DragDropContext onDragEnd={this.onDragEnd}>
+				<div class="gridContainer">
+					<div class="ribbon">This is the ribbon</div>
+					<div class="courses">Course list here</div>
+					<div class="sem-1">Semester 1</div>
+					<div class="sem-2">Semester 2</div>
+					<div class="sem-3">Semester 3</div>
+					<div class="sem-4">Semester 4</div>
+					<div class="sem-5">Semester 5</div>
+					<div class="sem-6">Semester 6</div>
+					<div class="sem-7">Semester 7</div>
+					<div class="sem-8">Semester 8</div>
+				</div>
+			</DragDropContext>
+		)
+	}
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
