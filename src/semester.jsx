068a1a53d8f192b4fc66db223d37fb136cacd736
ForgeThrall <forgemastersemail@gmail.com>
2021-04-07 17:01:09 -0500

    rename column to semester

--- src/semester.jsx
+++ src/semester.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { Droppable } from 'react-beautiful-dnd';
 import Course from './course';
 
-export default class Column extends React.Component {
+export default class Semester extends React.Component {
 	render() { return (
-		<div className={this.props.sem.id+' semesterContainer'}>
+		<div className={this.props.sem.id+' semester'}>
 			<div className="title">{this.props.sem.title}</div>
 			<Droppable droppableId={this.props.sem.id}>
 				{provided => (
