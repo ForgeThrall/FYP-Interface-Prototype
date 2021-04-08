1e9b92fdccb0ee047ba9ea0b180124d9cf5a6d8b
ForgeThrall <forgemastersemail@gmail.com>
2021-04-08 11:07:41 -0500

    remove is-svg vulnerability

--- src/availableCourses.jsx
+++ src/availableCourses.jsx
@@ -6,7 +6,7 @@ export default class AvailableCourses extends React.Component {
 	render() { return (
 		<div className={'availableCourses'}>
 			<div className="title">Available Courses</div>
-			<div style={{background: "white"}}>Textbox</div>
+			<input type="text" placeholder="Course Search"/>
 			<Droppable droppableId={'availableCourses'}>
 				{provided => (
 					<div ref={provided.innerRef} {...provided.droppableProps} className="courseField">
